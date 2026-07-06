b6f58ffd96d431f2ece36342a1995f545dd11266
Ivan E. Pais <ivan.e.pais@gmail.com>
2026-07-05 21:52:37 -0300

    chore: github

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/today-app/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
